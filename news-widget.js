34a4fb04c893a19ede417a020b2384709896bc5a
EmillioHezekiah <122629935+EmillioHezekiah@users.noreply.github.com>
2024-10-18 11:38:37 +0700

    Update news-widget.js

--- news-widget.js
+++ news-widget.js
@@ -212,7 +212,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 widget.innerHTML = `
                     <div class="modal-content" style="padding: 16px;">
-                        ${imgSrc ? `<img src="${imgSrc}" class="center-block" style="max-width: 45%; height: auto; display: block; margin-left: auto; margin-right: auto;" />` : ''}
+                        ${imgSrc ? `<img src="${imgSrc}" class="center-block" style="max-width: 30%; height: auto; display: block; margin-left: auto; margin-right: auto;" />` : ''}
                         <h2 style="font-family: 'Droid Serif'; font-size: 16pt; color: #4D4D4D;">${title}</h2>
                         <div class="modal-description" style="font-size: 14pt; font-family: 'Poppins'; text-align: justify;">${articleContent}</div>
                         <button class="close-modal" style="background: none; border: none; font-weight: bold; color: #840d0d; font-family: 'Roboto Condensed'; font-size: 14pt;">Back</button>
