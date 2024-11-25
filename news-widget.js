674d114d04340f739279875161d63fb636a92389
EmillioHezekiah <122629935+EmillioHezekiah@users.noreply.github.com>
2024-11-25 14:40:04 +0700

    Update news-widget.js

--- news-widget.js
+++ news-widget.js
@@ -186,7 +186,7 @@ document.addEventListener('DOMContentLoaded', function () {
                         <h1>${title}</h1>
                         ${imgSrc ? `<img src="${imgSrc}" alt="${title}" class="news-modal-image">` : ''}
                         <div>${articleContent}</div>
-                        <button id="back-to-news">Back</button>
+                        <button id="backButton">Back to News List</button>
                     </div>
                 `;
 
