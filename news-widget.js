e36f886b858b82bb51443149319c8b334987a78e
EmillioHezekiah <122629935+EmillioHezekiah@users.noreply.github.com>
2024-12-17 10:28:54 +0700

    Update news-widget.js

--- news-widget.js
+++ news-widget.js
@@ -96,7 +96,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     let imgSrc = imgElement ? correctImageUrl(imgElement.src) : '';
                     if (shouldExcludeImage(imgSrc)) imgSrc = '';
 
-                    const correctedLink = link.replace(/https:\/\/emilliohezekiah.github.io/, 'https://www.tradepr.work');
+                    const correctedLink = link.replace(/https:\/\/emilliohezekiah.github.io/, 'https://embed.tradepr.work');
                     const postedMetaDataElement = article.querySelector('.posted_meta_data');
                     const { postedDate, postedAuthor } = extractPostedMetaData(postedMetaDataElement);
 
@@ -113,9 +113,6 @@ document.addEventListener('DOMContentLoaded', function () {
                     newsContent.appendChild(newsItem);
                 });
 
-                const paginationElement = doc.querySelector('.pagination');
-                totalPages = paginationElement ? Math.max(...[...paginationElement.querySelectorAll('a')].map(a => parseInt(a.textContent.trim(), 10)).filter(Number)) : 1;
-
                 addPagination(currentPage);
                 togglePagination();
                 window.scrollTo(0, 0); // Scroll to the top of the page
@@ -129,6 +126,7 @@ document.addEventListener('DOMContentLoaded', function () {
         paginationContainer.id = 'pagination';
         paginationContainer.innerHTML = '';
 
+        // Pagination logic
         if (totalPages > 1) {
             if (currentPage > 1) {
                 const prevButton = document.createElement('span');
@@ -185,13 +183,11 @@ document.addEventListener('DOMContentLoaded', function () {
                     <div class="modal-content">
                         <h1 class="full-news-content-judul">${title}</h1>
                         ${imgSrc ? `<img src="${imgSrc}" alt="${title}" class="modal-thumbnail">` : ''}
-                        
                         <div class="full-news-content-paragraf">${articleContent}</div>
                         <button id="back-to-news">Back to News List</button>
                     </div>
                 `;
 
-                // Scroll to the top of the content
                 window.scrollTo(0, 0);
 
                 document.getElementById('back-to-news').onclick = () => loadNewsList(currentPage);
